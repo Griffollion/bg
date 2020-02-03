0076d6c80dae90a604323e70b2afe21de3448959
Griffollion <philippovichdenis@gmail.com>
2020-02-03 12:44:29 +0300

    default code end

--- js/gallery.js
+++ js/gallery.js
@@ -14,7 +14,7 @@
         // });
 
         window._ = new Glider(document.querySelector("[data-js='gallery-portrait']"), {
-            slidesToShow: 5,
+            slidesToShow: 1,
             slidesToScroll: 1,
             draggable: true,
             scrollLock: false,
@@ -22,7 +22,21 @@
             arrows: {
                 prev: "#glider-prev",
                 next: "#glider-next"
-            }
+            },  responsive: [
+                {
+                  // screens greater than >= 775px
+                  breakpoint: 767,
+                  settings: {
+                    slidesToShow: 3,
+                  }
+                },
+                {
+                    breakpoint: 1200,
+                    settings: {
+                      slidesToShow: 5,
+                    }
+                  }
+              ]
         });
 
         window._ = new Glider(document.querySelector("[data-js='gallery']"), {
