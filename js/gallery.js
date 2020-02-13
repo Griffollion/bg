7e21ae4c93c95a5cee034e57969a76bad05a4b49
Griffollion <philippovichdenis@gmail.com>
2020-02-13 10:30:33 +0300

    add gallery and replace arrow icons

--- js/gallery.js
+++ js/gallery.js
@@ -1,55 +1,69 @@
 document.addEventListener("DOMContentLoaded", function (event) {
-        // document.querySelector('.glider').addEventListener('glider-slide-visible', function(event){
-        //     var glider = Glider(this);
-        //     console.log('Slide Visible %s', event.detail.slide)
-        // });
-        // document.querySelector('.glider').addEventListener('glider-slide-hidden', function(event){
-        //     console.log('Slide Hidden %s', event.detail.slide)
-        // });
-        // document.querySelector('.glider').addEventListener('glider-refresh', function(event){
-        //     console.log('Refresh')
-        // });
-        // document.querySelector('.glider').addEventListener('glider-loaded', function(event){
-        //     console.log('Loaded')
-        // });
+  // document.querySelector('.glider').addEventListener('glider-slide-visible', function(event){
+  //     var glider = Glider(this);
+  //     console.log('Slide Visible %s', event.detail.slide)
+  // });
+  // document.querySelector('.glider').addEventListener('glider-slide-hidden', function(event){
+  //     console.log('Slide Hidden %s', event.detail.slide)
+  // });
+  // document.querySelector('.glider').addEventListener('glider-refresh', function(event){
+  //     console.log('Refresh')
+  // });
+  // document.querySelector('.glider').addEventListener('glider-loaded', function(event){
+  //     console.log('Loaded')
+  // });
 
-        window._ = new Glider(document.querySelector("[data-js='gallery-portrait']"), {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            draggable: true,
-            scrollLock: false,
-            rewind: true,
-            arrows: {
-                prev: "#glider-prev",
-                next: "#glider-next"
-            },  responsive: [
-                {
-                  // screens greater than >= 775px
-                  breakpoint: 767,
-                  settings: {
-                    slidesToShow: 3,
-                  }
-                },
-                {
-                    breakpoint: 1200,
-                    settings: {
-                      slidesToShow: 5,
-                    }
-                  }
-              ]
-        });
+  window._ = new Glider(document.querySelector("[data-js='gallery-portrait']"), {
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    draggable: true,
+    scrollLock: false,
+    rewind: true,
+    arrows: {
+      prev: "#glider-prev",
+      next: "#glider-next"
+    }, responsive: [
+      {
+        // screens greater than >= 775px
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 4,
+        }
+      },
+      {
+        breakpoint: 1800,
+        settings: {
+          slidesToShow: 5,
+        }
+      }
+    ]
+  });
 
-        window._ = new Glider(document.querySelector("[data-js='gallery']"), {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            draggable: true,
-            scrollLock: false,
-            dots: '#dots-2',
-            rewind: true,
-            arrows: {
-                prev: '#glider-prev-2',
-                next: '#glider-next-2'
-            }
-        });
+  window._ = new Glider(document.querySelector("[data-js='gallery']"), {
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    draggable: true,
+    scrollLock: false,
+    dots: '#dots-2',
+    rewind: true,
+    arrows: {
+      prev: '#glider-prev-2',
+      next: '#glider-next-2'
+    }
+  });
 
-      });
+});
+
+window.addEventListener("load", function (event) {
+  lightGallery(document.querySelector('#lightgallery .glider-track'));
+})
+
+
+
+
